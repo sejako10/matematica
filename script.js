d2d89086e7990599828fc7696634eecdef53c1ae
sejako <sejako6074@tutamail.com>
2025-03-15 01:15:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const rules = document.querySelectorAll(".rules li");
-    
-    rules.forEach(rule => {
-        rule.addEventListener("mouseover", function() {
-            rule.style.color = "#FFA500";
-        });
-        
-        rule.addEventListener("mouseout", function() {
-            rule.style.color = "white";
-        });
-    });
-});
+
