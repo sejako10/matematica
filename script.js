88499bd26b967458e8d57d88f7c8b5b43c33a6ab
sejako <sejako6074@tutamail.com>
2025-03-15 00:32:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-document.querySelector('.regulament').addEventListener('mouseover', function() {
-    this.style.transform = 'scale(1.02)';
-    this.style.boxShadow = '0px 0px 15px rgba(255, 165, 0, 0.5)';
-});
-
-document.querySelector('.regulament').addEventListener('mouseout', function() {
-    this.style.transform = 'scale(1)';
-    this.style.boxShadow = 'none';
+document.addEventListener("DOMContentLoaded", function() {
+    const rules = document.querySelectorAll(".rules li");
+    
+    rules.forEach(rule => {
+        rule.addEventListener("mouseover", function() {
+            rule.style.color = "#FFA500";
+        });
+        
+        rule.addEventListener("mouseout", function() {
+            rule.style.color = "white";
+        });
+    });
 });
